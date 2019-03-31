9b95e2e3573b7a9c841ca7ea7c656aba740cd95f
cl1190 <46169691+cl1190@users.noreply.github.com>
2019-03-30 21:05:03 -0400

    CarlyA.py
    RPS script

---
+++ carlyA.py
@@ -0,0 +1,19 @@
+## Simple, bare-bones version of  CarlyB
+
+import random
+
+class rps():
+    ### initializing function ###
+    def __init__(self):
+        return
+
+    def shoot(self, last_round):
+        options = ['R', 'P', 'S']
+        if last_round == 'R':
+            return('P')
+        elif last_round == 'P':
+            return('S')
+        elif last_round == 'S':
+            return('R')
+        else:
+            return(random.choice(options))
